989e65e74d7c9128c60585f5e423026474eb71e6
hatukanezumi <hatuka@nezumi.nu>
2009-06-14 06:38:33 +0000

    initial import

---
+++ EUCJPASCII.pm
@@ -0,0 +1,23 @@
+package Encode::EUCJPASCII;
+our $VERSION = "0.01";
+ 
+use Encode;
+use XSLoader;
+XSLoader::load(__PACKAGE__,$VERSION);
+
+1;
+__END__
+
+=head1 NAME
+ 
+Encode::EUCJPASCII - New Encoding
+ 
+=head1 SYNOPSIS
+
+You got to fill this in!
+
+=head1 SEE ALSO
+
+L<Encode>
+
+=cut
